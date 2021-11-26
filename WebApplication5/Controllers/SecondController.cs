999a86e33f852e719e41ef847246a987c2ee9a73
Anamika-s <anamika.sawhney22@gmail.com>
2021-11-26 07:24:16 +0000

    MVc Demos

---
+++ WebApplication5/Controllers/SecondController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ContentResult Index1()
+        {
+            return Content("This is the content to be returned");
+        }
+        public string Index2()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ WebApplication5/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+namespace WebApplication5.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult GetDetails()
+        {
+            Student student = new Student()
+            { Id = 1, Name = "Deepak", BatchCode = "B001", Marks = 90 };
+            ViewBag.student = student;
+            return View();
+        }
+        public ActionResult GetDetails2()
+        {
+            Student student = new Student()
+            { Id = 1, Name = "Deepak", BatchCode = "B001", Marks = 90 };
+            ViewData["student"] = student;
+            return View();
+        }
+
+        public ActionResult List1()
+        {
+            List<Student> list = new List<Student>()
+            {
+                 new Student()  { Id = 1, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 2, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 3, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 4, Name = "Deepak", BatchCode = "B001", Marks = 90 }
+            };
+            ViewBag.students = list;
+            return View();
+        }
+
+        public ActionResult List2()
+        {
+            List<Student> list = new List<Student>()
+            {
+                 new Student()  { Id = 1, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 2, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 3, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 4, Name = "Deepak", BatchCode = "B001", Marks = 90 }
+            };
+            ViewBag.students = list;
+            return View();
+        }
+        public ActionResult List3()
+        {
+            List<Student> list = new List<Student>()
+            {
+                 new Student()  { Id = 1, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 2, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 3, Name = "Deepak", BatchCode = "B001", Marks = 90 },
+                 new Student()  { Id = 4, Name = "Deepak", BatchCode = "B001", Marks = 90 }
+            };
+            ViewData["students"] = list;
+            return View();
+        }
+    }
+}
